ee2a6515c7540f6ac9e440e4006f093f2b2e9524
elijahmm17 <elijahmm17@gmail.com>
2019-07-17 21:19:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  session[:session_secret] = {}
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    session[:session_secret][:item] = params[:item]
+    erb :display_item
+  end 
 end
